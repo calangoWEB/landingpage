c58e962b4033b7deb886c0e83132a000d09bbcb4
Joabsonlg <deyvissonaislan@outlook.com>
2020-06-01 09:49:06 -0300

    intro

---
+++ src/components/Intro.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container class="pa-0" style="display:contents">
+    <v-parallax
+      dark
+      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    >
+      <v-row align="center" justify="center">
+        <v-col class="text-center" cols="12">
+          <h1 class="display-1 font-weight-thin mb-4">Vuetify.js</h1>
+          <h4 class="subheading">Build your application today!</h4>
+        </v-col>
+      </v-row>
+    </v-parallax>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Intro",
+};
+</script>
---
+++ src/components/Sobre.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <h1 class="display-1 font-weight-light">Seção Sobre</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+          perspiciatis quidem saepe repellendus non. Quod fuga, nostrum qui
+          obcaecati ratione quisquam ullam tenetur pariatur ab facilis iure est,
+          consequatur quae!
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Sobre",
+};
+</script>
